61fb0db89f43ecb6f8820a0cc3c15762a4517f4d
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-15 17:16:24 +0200

    Add (getPostByID) feature to (useAppState) hook

--- src/hooks/useAppState.tsx
+++ src/hooks/useAppState.tsx
@@ -1,17 +1,24 @@
+import { PostType } from '../services';
 import { useSelector } from './useTypedStore';
 
 export const useAppState = () => {
   const { users } = useSelector((state) => state.users);
-  users ? users : null;
 
   const { posts, loading, error } = useSelector((state) => state.posts);
 
-  // posts ? posts : null;
+  const getPostById = (postId: string): PostType => {
+    let post = posts.find((post) => {
+      return post.id === postId;
+    });
+
+    return post!;
+  };
 
   return {
-    usersState: users,
+    usersState: users!,
     postsList: posts,
     postsLoading: loading,
     postsError: error,
+    getPostById: getPostById,
   };
 };
