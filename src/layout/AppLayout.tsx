1a700c6bbd3a504958506c100007174d3df620eb
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-16 13:59:15 +0200

    Add HeaderNavbar layout

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import HeaderNav from './HeaderNavbar';
 
 const AppLayout = () => {
   return (
     <div className="min-h-screen min-w-full">
       <div className="container mx-auto">
         {/* {===-> HEADER <-===} */}
-
+        <HeaderNav />
         {/* {===-> MAIN-BODY <-===} */}
 
         {/* {===->> MAIN-CONTENT <<-===} */}
