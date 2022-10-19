785fa1617f880c482f5991c64b42cf308b206dcf
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-19 20:24:41 +0200

    Fix Commit conflict && minor fixes

--- src/layout/MainBody.tsx
+++ src/layout/MainBody.tsx
@@ -5,7 +5,7 @@ import MainSidebar from './MainSidebar';
 const MainBody = () => {
   return (
     <>
-      <div className="container mx-auto flex flex-col md:flex-row  md:space-x-4 2xl:mt-5 sm-only:space-y-4">
+      <div className="container mx-auto flex flex-col md:flex-row  md:space-x-4 sm-only:space-y-4">
         {/* {===->> MAIN-CONTENT <<-===} */}
         <div className=" md:w-8/12">
           <MainSection />
