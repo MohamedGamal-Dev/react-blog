4666854756e210c39a8e9bb329ba13e5a00f1d09
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-19 21:12:20 +0200

    Add initial styles to CatchAll page

--- src/layout/CatchAll.tsx
+++ src/layout/CatchAll.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-
+import { useNavigate, Link } from 'react-router-dom';
+import { CardFrame } from '../components';
 function CatchAll() {
+  // const navigate = useNavigate();
+
   return (
-    <div>
-      {'404'}
-      <br />
-      {'Ops!!! , Sorry, the page you are looking for could not be found.'}
-    </div>
+    <>
+      <CardFrame>
+        <div className="text-lg font-medium text-mgLight-neutral sm:px-10 lg:text-xl xl:px-48">
+          {'Sorry, the page you are looking for could not be found.'}
+        </div>
+
+        <button className="mt-5 w-full rounded-lg bg-mgLight-accent px-5 py-2.5 text-center text-lg font-bold text-white shadow-md shadow-mgLight-success hover:bg-mgLight-success hover:shadow-mgLight-accent  focus:outline-none">
+          <Link to={'/'}> BACK TO HOMEPAGE </Link>
+        </button>
+      </CardFrame>
+    </>
   );
 }
 
