7c2065315de965e7be9931ca08a5ae66d14d78a1
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-20 16:07:52 +0200

    Change PostsList order to show new post first

--- src/pages/PostsList.tsx
+++ src/pages/PostsList.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { useSelector } from '../hooks/useTypedStore';
 import { PostType } from '../services';
 import PostCard from './PostCard';
 
 const PostsList: React.FunctionComponent = () => {
-
   const { posts, loading, error } = useSelector((state) => state.posts);
 
+  // Sorting (POSTS) to Show New POST @ TOP
+  const sortedPosts = posts.slice().sort((one, two) => {
+    // console.log(one.date);
+    // console.log(two.date);
+    return one.date! > two.date! ? -1 : 1;
+  });
+
+  // console.log(posts, '=== POSTS ===');
+  // console.log('***', sortedPosts, '***');
+
   const renderPostsList = () => {
-    return posts.map((post: PostType) => {
+    return sortedPosts.map((post: PostType) => {
       return (
         <React.Fragment key={post.id}>
           <PostCard post={post} />
@@ -33,7 +42,6 @@ const PostsList: React.FunctionComponent = () => {
     <>
       {/* <h2>Posts List</h2> */}
       <div className="flex-1 flex-col space-y-4">{renderPostsOption()}</div>
-      {/* <DummyLongP /> */}
     </>
   );
 };
