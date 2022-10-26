0f41dc3d2bd0c0c2f27af37facd3c87ab753390d
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-10-26 10:32:22 +0200

    Apply UI-Card to Post Card/Render

--- src/pages/PostCard.tsx
+++ src/pages/PostCard.tsx
@@ -1,115 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-import { useActions } from '../hooks/useActions';
+import { Card } from '../components';
 import { PostProps } from '../services';
+import { useActions } from '../hooks/useActions';
 import PostAuthor from './PostAuthor';
 import PostDate from './PostDate';
-import PostRender from './PostRender';
+import PostFooter from './PostFooter';
 
 const PostCard: React.FunctionComponent<PostProps> = ({ post }) => {
   const { deletePost } = useActions();
   const { userId, id, title, body, date } = post;
 
-  const handleDeleteClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
+  const handleDeleteClick = () => {
     deletePost(id);
   };
-  const handlePostRender = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    <PostRender />;
-  };
 
-  const renderPostCard = () => {
-    return (
-      <>
-        {/* { MAIN POST-CARD Frame } */}
-        <div className="mx-auto rounded-lg  border border-mgLight-secondary/20 bg-mgLight-primary px-1 pt-1 shadow shadow-mgLight-secondary ">
-          <div className="rounded-lg bg-mgLight-base-100 py-4 px-6">
-            {/* { *** TITLE && POST OPTIONS *** } */}
-            <div className="flex items-center justify-between">
-              <Link
-                to={`/post/${id}`}
-                className="text-2xl font-bold text-mgLight-neutral hover:text-mgLight-accent hover:underline"
-              >
-                {title}
-              </Link>
-
-              <div className="flex items-center space-x-1">
-                <Link
-                  to={`/post/${id}/edit`}
-                  className="transform cursor-pointer rounded bg-mgLight-accent px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-success"
-                >
-                  Edit
-                </Link>
-
-                <button
-                  onClick={handleDeleteClick}
-                  className="transform cursor-pointer rounded bg-mgLight-error px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-error/75"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-
-            {/* { *** Author && DATE *** } */}
-            <div className="flex items-start space-x-2 border-b-2 border-mgLight-secondary pb-1">
-              <div className="font-serif text-sm font-medium text-mgLight-accent">
-                <PostAuthor userId={userId} />
-              </div>
-              <div className="text-sm font-light text-mgLight-neutral ">
-                {/* { *** DATE *** } */}
-                <PostDate timestamp={date!} />
-              </div>
-            </div>
-
-            {/* { *** SUMMARY BODY *** } */}
-            <div className="mt-0">
-              <p className="mt-2 text-mgLight-neutral/90 ">
-                {body.substring(0, 500)}
-              </p>
-            </div>
-          </div>
-
-          {/* { *** CARD-FOOTER *** } */}
-          <div className="flex items-center justify-between rounded-bl-lg rounded-br-lg bg-mgLight-primary py-3 px-6">
-            <div className="flex items-center space-x-1">
-              {' '}
-              {/* { reaction-feature-placeholder } */}
-            </div>
-            <button onClick={handlePostRender}>
-              <Link
-                to={`/post/${id}`}
-                className="font-medium text-mgLight-base-100  hover:text-mgLight-accent"
-              >
-                Read More ⟶
-              </Link>
-            </button>
-          </div>
-        </div>
-
-        {/* { Categories/tags-possible-feature } */}
-        {/* <div className="mt-4 flex items-center justify-start space-x-1 ">
-            <Link
-              to={'/'}
-              className="transform cursor-pointer rounded bg-mgLight-primary px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-success"
-            >
-              Categories
-            </Link>
-            <Link
-              to={'/'}
-              className="transform cursor-pointer rounded bg-mgLight-primary px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-success"
-            >
-              Tags
-            </Link>
-          </div> */}
-      </>
-    );
-  };
-
-  return <>{renderPostCard()}</>;
+  return (
+    <>
+      <Card
+        frameFooterRenderComponent={<PostFooter id={id} type={'list'} />}
+        authorRender={<PostAuthor userId={userId} />}
+        dateRender={<PostDate timestamp={date!} />}
+        onDeleteClick={handleDeleteClick}
+        headingText={title}
+        headingLink={true}
+        targetId={id}
+      >
+        {body.substring(0, 500)}
+        {`...`}
+      </Card>
+    </>
+  );
 };
 
 export default PostCard;
--- src/pages/PostRender.tsx
+++ src/pages/PostRender.tsx
@@ -1,98 +1,43 @@
 import React from 'react';
-import { Link, useParams, useNavigate, Navigate } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
+import { HOME_PAGE, POST_BY_ID_EDIT_PROP } from '../routes';
 import { useAppState } from '../hooks/useAppState';
 import { useActions } from '../hooks/useActions';
+import { Card } from '../components/ui/';
+
 import PostAuthor from './PostAuthor';
+import PostDate from './PostDate';
+import PostFooter from './PostFooter';
 
 const PostRender: React.FunctionComponent = () => {
   const { deletePost } = useActions();
   const navigate = useNavigate();
-  let { postId } = useParams();
-  const { postsList } = useAppState();
+  const { postId } = useParams();
+  const { getPostById } = useAppState();
 
-  // NOTE: n16
-  const post = postsList.find((post) => {
-    return post.id === postId;
-  });
+  const post = getPostById(postId!);
+  const { userId, id, title, body, date } = post;
 
-  if (!post) {
-    return (
-      <>
-        {'Sorry!!!, Post Not Found'}
-        {/* <Navigate to="/" /> */}
-      </>
-    );
-  }
-
-  const { userId, id, title, body } = post;
-
-  const handleDeleteClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
+  const handleDeleteClick = () => {
     deletePost(id);
-    navigate('/');
+    navigate(HOME_PAGE);
   };
 
-  const renderFullPost = () => {
-    return (
-      <>
-        {/* { MAIN POST-CARD Frame } */}
-        <div className="mx-auto rounded-lg  border border-mgLight-secondary/20 bg-mgLight-primary px-1 pt-1 shadow shadow-mgLight-secondary ">
-          <div className="rounded-lg bg-mgLight-base-100 py-4 px-6">
-            {/* { *** TITLE && POST OPTIONS *** } */}
-            <div className="flex items-center justify-between">
-              <h2 className="text-2xl font-bold text-mgLight-neutral hover:text-mgLight-accent hover:underline md:text-3xl">
-                {title}
-              </h2>
-
-              <div className="flex items-center space-x-1">
-                <Link
-                  to={`/post/${id}/edit`}
-                  className="transform cursor-pointer rounded bg-mgLight-accent px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-success"
-                >
-                  Edit
-                </Link>
-
-                <button
-                  onClick={handleDeleteClick}
-                  className="transform cursor-pointer rounded bg-mgLight-error px-3 py-1 text-sm font-bold text-white transition-colors duration-200 hover:bg-mgLight-error/75"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-
-            {/* { *** Author && DATE *** } */}
-            <div className="flex items-start space-x-2 border-b-2 border-mgLight-secondary pb-1">
-              <div className="font-serif text-sm font-medium text-mgLight-accent">
-                <PostAuthor userId={userId} />
-              </div>
-              <div className="text-sm font-light text-mgLight-neutral ">
-                {/* { date-feature-placeholder } */}
-                Feb 02, 2022
-              </div>
-            </div>
-
-            {/* { *** BODY CONTENT *** } */}
-            <div className="mt-0">
-              <p className="mt-2 text-lg text-mgLight-neutral/90 ">{body}</p>
-            </div>
-          </div>
-
-          {/* { *** Post-FOOTER *** } */}
-          <div className="flex items-center justify-between rounded-bl-lg rounded-br-lg bg-mgLight-primary py-3 px-6">
-            <div className="flex items-center space-x-1">
-              {' '}
-              {/* { reaction-feature-placeholder } */}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  return <>{renderFullPost()}</>;
+  return (
+    <>
+      <Card
+        frameFooterRenderComponent={<PostFooter type={'single'} />}
+        authorRender={<PostAuthor userId={userId} />}
+        dateRender={<PostDate timestamp={date!} />}
+        onDeleteClick={handleDeleteClick}
+        headingText={title}
+        targetId={id}
+      >
+        {body}
+      </Card>
+    </>
+  );
 };
 
 export default PostRender;
