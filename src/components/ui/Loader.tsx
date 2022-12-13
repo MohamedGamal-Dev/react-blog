d8ff8170d5903dd5010c522a723a308b85021913
Mohamed Gamal <MohamedGamal.Dv@gmail.com>
2022-12-13 13:42:00 +0200

    Add basic Loader spinner component

---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <>
+      <div className="h-20 w-20 animate-spin rounded-full border-[1rem] border-t-[1rem] border-mgLight-accent border-t-mgLight-primary"></div>
+    </>
+  );
+};
+
+export default Loader;
